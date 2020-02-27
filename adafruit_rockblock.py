cdca40abd84dc577da646c84c40e901d81878f19
caternuson <caternuson@gmail.com>
2020-02-27 15:31:26 -0800

    cleaning up crumbs

--- adafruit_rockblock.py
+++ adafruit_rockblock.py
@@ -33,18 +33,13 @@ Implementation Notes
 
 **Hardware:**
 
-.. todo:: Add links to any specific hardware product page(s), or category page(s). Use unordered list & hyperlink rST
-   inline format: "* `Link Text <url>`_"
+* `RockBLOCK 9603 Iridium Satellite Modem <https://www.adafruit.com/product/4521>`_
 
 **Software and Dependencies:**
 
 * Adafruit CircuitPython firmware for the supported boards:
   https://github.com/adafruit/circuitpython/releases
 
-.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies based on the library's use of either.
-
-# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
-# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
 # imports
