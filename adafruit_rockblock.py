c383cc20664ba4e00c7f926b03e191bccac2d1b3
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:14 -0400

    Update version string

--- adafruit_rockblock.py
+++ adafruit_rockblock.py
@@ -29,7 +29,7 @@ Implementation Notes
 import time
 import struct
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_RockBlock.git"
 
 
